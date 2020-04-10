ff94d8c9cbbc70215efaabf8133a0656a883d7e4
Yifan Zhao <30790781+Eric-Zzz@users.noreply.github.com>
2020-04-10 19:42:08 +1000

    Style update (#7)
    test github authority
    
    merge master
    
    update .gitignore
    
    commit before updating from master
    
    init and update css style on HomePage and FormPage
    
    reformat warning

--- src/views/FormPage/components/Form.js
+++ src/views/FormPage/components/Form.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { Form, Input, Button, Select } from "antd";
+import React from "react";
+import { Button, Card, Form, Input, Select } from "antd";
 import { v4 as uuidv4 } from "uuid";
 import { defaultTask } from "../../../defaultTasks";
 import { defaultQuestion } from "../../../defaultQuestions";
 import { useHistory } from "react-router-dom";
+import "../FormPage.css";
 
 const { Option } = Select;
+const { TextArea } = Input;
 const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 const daysOptions = DAYS.map((o, index) => (
   <Option value={o} key={o}>
@@ -25,9 +27,21 @@ const FormLayout = () => {
         id: uuidv4(),
         author: values["author"],
         content: [
-          { id: uuidv4(), question: "1", answer: values["answer1"] },
-          { id: uuidv4(), question: "2", answer: values["answer2"] },
-          { id: uuidv4(), question: "3", answer: values["answer3"] },
+          {
+            id: uuidv4(),
+            question: defaultQuestion[0],
+            answer: values["answer1"],
+          },
+          {
+            id: uuidv4(),
+            question: defaultQuestion[1],
+            answer: values["answer2"],
+          },
+          {
+            id: uuidv4(),
+            question: defaultQuestion[2],
+            answer: values["answer3"],
+          },
         ],
       },
     ];
@@ -36,8 +50,11 @@ const FormLayout = () => {
     history.push("/");
   };
   return (
-    <div>
+    <Card className="form-card">
+      <h1>Personal Daily Report</h1>
       <Form
+        labelCol={{ span: 4 }}
+        labelAlign="left"
         form={form}
         onFinish={onFinish}
         initialValues={{
@@ -69,17 +86,17 @@ const FormLayout = () => {
         >
           <Select>{daysOptions}</Select>
         </Form.Item>
-        {defaultQuestion[0]}
+        <h3>{defaultQuestion[0]}</h3>
         <Form.Item label="Answer" name="answer1">
-          <Input />
+          <TextArea rows={4} />
         </Form.Item>
-        {defaultQuestion[1]}
+        <h3>{defaultQuestion[1]}</h3>
         <Form.Item label="Answer" name="answer2">
-          <Input />
+          <TextArea rows={4} />
         </Form.Item>
-        {defaultQuestion[2]}
+        <h3>{defaultQuestion[2]}</h3>
         <Form.Item label="Answer" name="answer3">
-          <Input />
+          <TextArea rows={4} />
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit">
@@ -87,7 +104,7 @@ const FormLayout = () => {
           </Button>
         </Form.Item>
       </Form>
-    </div>
+    </Card>
   );
 };
 
--- src/views/HomePage/components/QuestionAnswer.js
+++ src/views/HomePage/components/QuestionAnswer.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row } from "antd";
+import "../HomePage.css";
 
 const QuestionAnswer = ({ content }) => {
   const renderQuestionAnswer = (content) => {
     return content.map((qA, index) => {
       return (
         <div key={index}>
-          <Row>{qA.question}</Row>
-          <Row>{qA.answer}</Row>
+          <Row className="question">{qA.question}</Row>
+          <Row className="answer">{qA.answer}</Row>
         </div>
       );
     });
