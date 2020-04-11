321c30cbc0a390c1917c9b7a9e9b6ec982beb8db
EricZzz <zyfhncn@gmail.com>
2020-04-11 12:34:00 +1000

    1.change taskCard text align 2.relocated delete button 3.add letter avatar

--- src/views/HomePage/components/TaskCard.js
+++ src/views/HomePage/components/TaskCard.js
@@ -1,22 +1,41 @@
 import React from "react";
-import { Avatar, Card, Button } from "antd";
+import { Avatar, Button, Card } from "antd";
 import QuestionAnswer from "./QuestionAnswer";
 import "../HomePage.css";
+import { CloseOutlined } from "@ant-design/icons";
+import RAvatar from "react-avatar";
 
 const { Meta } = Card;
 const Task = ({ task, handleDelete }) => {
   return (
-    <Card className="qa-card">
-      <Meta
-        avatar={
-          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        }
-        title={task.author}
-      />
-      <br />
+    <Card
+      className="qa-card"
+      bordered={false}
+      title={
+        <Meta
+          avatar={<RAvatar name={task.author} size={32} round="20px" />}
+          title={task.author}
+        />
+      }
+      extra={
+        <Button className="del-btn" onClick={() => handleDelete(task.id)}>
+          <CloseOutlined />
+        </Button>
+      }
+    >
       <QuestionAnswer content={task.content} />
-      <Button onClick={() => handleDelete(task.id)}>Delete</Button>
     </Card>
+    // <Card className="qa-card">
+    //   <Meta
+    //     avatar={
+    //       <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    //     }
+    //     title={task.author}
+    //   />
+    //   <br />
+    //   <QuestionAnswer content={task.content} />
+    //   <Button onClick={() => handleDelete(task.id)}>Delete</Button>
+    // </Card>
   );
 };
 
